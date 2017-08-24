ee5053a1e0956e6371a82a3dbd3d3bcc950b017b
Mallory Harding <brian.harding@gmail.com>
2017-08-23 22:55:28 -0400

    first

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+
+
+
+// Get all characters
+app.get("/all", function(req, res) {
+  res.json(characters);
+});
+
+require('./app/routing/htmlRoutes.js')(app);
+require('./app/routing/apiRoutes.js')(app);
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
